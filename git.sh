9c2db47c0943a26a9fca55bb705b65d1bfadb70d
于志远 <lhyfe1987@163.com>
2016-04-18 16:54:32 +0800

    install -y

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@
 cd ~/
 #安装git
 echo "开始安装git 2.7.4"
-yum install curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel perl-devel wget
+yum install curl curl-devel zlib-devel openssl-devel perl cpio expat-devel gettext-devel perl-devel wget -y
 wget https://github.com/git/git/archive/v2.7.4.tar.gz
 tar -zxvf v2.7.4.tar.gz
 cd git-2.7.4/
